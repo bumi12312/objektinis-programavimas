695f461561bc0fcfc3d58256a0581ae67f17f816
bumi12312 <paluckasv@gmail.com>
2026-09-23 13:09:30 +0300

    Update pirmas.cpp

--- pirmas.cpp
+++ pirmas.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
 
+using namespace std;
+
+struct studentas {
+    string vardas = "";
+    string pavarde = "";
+    int nd = 0;
+    int egz = 0;
+    float galutinis = 0;
+};
+
+int main()
+{
+  
