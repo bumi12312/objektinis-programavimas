a63a728e2f1d5d774084d184b46074687ce7ab5c
bumi12312 <paluckasv@gmail.com>
2026-09-23 13:42:22 +0300

    Update pirmas.cpp

--- pirmas.cpp
+++ pirmas.cpp
@@ -18,7 +18,7 @@ int main()
     int studSk;
     cout << "Studentu skaičius: ";
     cin >> studSk;
-    Studentas s[100];
+    studentas s[100];
     for(int i=0;i<studSk;i++) {
         cout << "\n" << (i+1) << "-as studentas\n";
         cout << "Vardas: ";
@@ -31,5 +31,17 @@ int main()
         for(int j=0;j<s[i].n;j++) {
             cout << (j+1) << "namu darbo balas: ";
             cin >> s[i].nd[j];
-            suma += s[i].nd[j];
+            sum += s[i].nd[j];
         }
+        double vidurkis=(double)sum/s[i].n;
+        cout << "Egzamino rezultatas: ";
+        cin >> s[i].egz;
+        s[i].galutinis=0.4*vidurkis+0.6*s[i].egz;
+    }
+    cout << "\n" << left << setw(10) << "Vardas" << setw(10) << "Pavarde" << "Galutinis\n";
+    cout << fixed << setprecision(2);
+    for(int i=0;i<studSk;i++) {
+        cout << left << setw(10) << s[i].vardas << setw(10) << s[i].pavarde << s[i].galutinis << "\n";
+    }
+    return 0;
+}
