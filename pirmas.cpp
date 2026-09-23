e0eb63d8a230c2bd86ee08752199b36edaba1da3
bumi12312 <paluckasv@gmail.com>
2026-09-23 13:19:35 +0300

    Update pirmas.cpp

--- pirmas.cpp
+++ pirmas.cpp
@@ -8,10 +8,19 @@ struct studentas {
     string vardas = "";
     string pavarde = "";
     int n = 0;
+    int nd = 0;
     int egz = 0;
     float galutinis = 0;
 };
 
 int main()
 {
-  
+    int studSk;
+    cout << "Studentu skaičius: ";
+    cin >> studSk;
+    Studentas s[100];
+    for(int i=0;i<studSk;i++) {
+        cout << "\n" << (i+1) << "-as studentas\n";
+        cout << "Vardas: ";
+        cin >> s[i].vardas;
+    
