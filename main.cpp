777c430c64b7ca322ee3bf9f650664cb6e56e688
bumi12312 <paluckasv@gmail.com>
2026-09-23 13:04:47 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
 
+using namespace std;
+
+struct studentas {
+    string vardas = "";
+    string pavarde = "";
+    int n = 0;
+    int egz = 0;
+    float galutinis = 0;
+    float vid = 0;
